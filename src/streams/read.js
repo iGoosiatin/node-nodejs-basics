cd1582e43f4658e4a6239b03bd4ce9b28d3540b4
Ilja Gusiatin <ilja.gusiatin@softeq.com>
2023-06-14 16:48:03 +0300

    feat: implement streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from "fs";
+
+import path from "path";
+import { getDirnameFromUrl } from "../utils/utils.js";
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const file = path.join(__dirname, "files", "fileToRead.txt");
+
+const readStream = createReadStream(file);
+
 const read = async () => {
-    // Write your code here 
+    readStream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from "fs";
+
+import path from "path";
+import { getDirnameFromUrl } from "../utils/utils.js";
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const file = path.join(__dirname, "files", "fileToWrite.txt");
+
+const writeStream = createWriteStream(file);
+
 const write = async () => {
-    // Write your code here 
+    process.stdin.pipe(writeStream);
 };
 
 await write();
