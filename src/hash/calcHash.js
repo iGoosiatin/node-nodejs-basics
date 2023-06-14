0bf7d5fe97d838b622ad629d32de93d515a40f82
Ilja Gusiatin <ilja.gusiatin@softeq.com>
2023-06-14 12:32:18 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import { createHash } from "crypto";
+import path from "path";
+import { getDirnameFromUrl } from "../utils/utils.js";
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const file = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256').update(file).digest('hex');
+    console.log(hash);
 };
 
 await calculateHash();
