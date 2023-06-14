ed53df5e3a93922bd7aeb68c21b0f60c4be6ea33
Ilja Gusiatin <ilja.gusiatin@softeq.com>
2023-06-14 18:50:51 +0300

    feat: implement Worker Threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { Worker } from "worker_threads";
+import path from "path";
+import { getDirnameFromUrl } from "../utils/utils.js";
+
+const CPUs = 8;
+const START_NUMBER = 10;
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const workerScript = path.join(__dirname, "worker.js");
+
+const fibonacciWorkerService = (number) => new Promise(resolve => {
+    const worker = new Worker(workerScript, { workerData: number });
+
+    worker.on('message', data => resolve({
+        status: "resolved",
+        data
+    }));
+
+    worker.on('error', () => resolve({
+        status: "error",
+        data: null
+    }));
+})
+
 const performCalculations = async () => {
-    // Write your code here
+    const workersPool = Array.from({length: CPUs}, (_, i) => fibonacciWorkerService(START_NUMBER + i));
+    const result = await Promise.all(workersPool);
+    console.log(result);
 };
 
 await performCalculations();
