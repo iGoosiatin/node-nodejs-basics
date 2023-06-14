5c5de68a2486408f5b59a2b5992ae78518143c7b
Ilja Gusiatin <ilja.gusiatin@softeq.com>
2023-06-14 10:42:28 +0300

    feat: complete fs/create.js subtask

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises"
+import path from "path";
+import { getDirnameFromUrl, checkIfExists } from "../utils/utils.js";
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const freshFile = path.join(__dirname, "files", "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+    const isExisting = await checkIfExists(freshFile);
+    if (isExisting) {
+        throw new Error ("FS operation failed")
+    } else {
+        await fs.writeFile(freshFile, "I am fresh and young");
+    }
 };
 
 await create();
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,18 @@
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { stat } from "fs/promises";
 
 export const getFilenameFromUrl = fileURLToPath;
 
 export const getDirnameFromUrl = (url) => dirname(fileURLToPath(url));
+
+export const checkIfExists = async (path) => {
+  let isExisting = false;
+  try {
+    await stat(path);
+    isExisting = true;
+  } catch {
+    isExisting = false;
+  }
+  return isExisting;
+}
