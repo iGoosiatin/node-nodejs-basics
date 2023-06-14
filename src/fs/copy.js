d5e6eb2604f578b6dfa7e3572615f4efbffe62b0
Ilja Gusiatin <ilja.gusiatin@softeq.com>
2023-06-14 11:05:05 +0300

    feat: complete fs/copy.js subtask

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from "fs/promises"
+import path from "path";
+import { getDirnameFromUrl, checkIfExists } from "../utils/utils.js";
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const sourceDir = path.join(__dirname, "files");
+const targetDir = path.join(__dirname, "files_copy")
+
 const copy = async () => {
-    // Write your code here 
+    const isSourceDirExisting = await checkIfExists(sourceDir);
+    if (!isSourceDirExisting) {
+        throw new Error ("FS operation failed");
+    }
+
+    const isTargetDirExisting = await checkIfExists(targetDir);
+    if (isTargetDirExisting) {
+        throw new Error ("FS operation failed");
+    }
+
+    await fs.cp(sourceDir, targetDir, { recursive: true });
 };
 
 await copy();
