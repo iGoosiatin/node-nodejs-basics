b8fc0f5394d915749d8c0d7367d926933cbffb61
Ilja Gusiatin <ilja.gusiatin@softeq.com>
2023-06-14 10:47:59 +0300

    feat: complete fs/delete.js subtask

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import fs from "fs/promises"
+import path from "path";
+import { getDirnameFromUrl } from "../utils/utils.js";
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const filetoRemove = path.join(__dirname, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.rm(filetoRemove);
+    } catch {
+        throw new Error ("FS operation failed");
+    }
 };
 
 await remove();
